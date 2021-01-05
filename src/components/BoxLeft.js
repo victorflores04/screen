3c0ff35a8078413b8a4dc4d5d58ef88005b9a053
Victor Flores <flores.vicf@gmail.com>
2021-01-05 03:09:25 -0500

    fixed web

--- src/components/BoxLeft.js
+++ src/components/BoxLeft.js
@@ -3,7 +3,7 @@ export const BoxLeft = () => {
     return (
         <div className="box__left">
             <div className="box__label">
-                <div className="box__label--text">fw20 Drop</div>
+                <div className="box__label--text">FW20 Drop</div>
                 <div className="box__label--textt">Is Out Now</div>
             </div>
             <div className="btn btn-p box__button">shop all</div>
@@ -12,7 +12,7 @@ export const BoxLeft = () => {
                     <img src="https://i.imgur.com/DJx3JVa.jpg" />
                 </div>
                 <div className="box__img-right">
-                    <img src="https://i.imgur.com/DJx3JVa.jpg" />
+                    <img src="https://i.imgur.com/8yl4VIS.jpg" />
                 </div>
             </div>
             <div className="box__block">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React,{ useState } from "react";
-import badge from '../assets/Bag.svg';
+//import badge from '../assets/Bag.svg';
 export const Header = () => {
     
     const [isNavVisible, setNavVisibility] = useState(true);
@@ -16,7 +16,7 @@ export const Header = () => {
             <a href="/" className="navbar__menu--links">women</a>
             <a href="/" className="navbar__menu--links">men</a>
             <a href="/" className="navbar__menu--links">about</a>
-            <a href="/" className="navbar__menu--icons"><img src={badge} className="navbar__badge" alt="badge"/></a>
+            <a href="/" className="navbar__menu--icons"><img src="https://i.imgur.com/gJ4wJWb.png" className="navbar__badge" alt="badge"/></a>
             </div>
           )}
           <button onClick={toggleNav} className="navbar__bars"><i className="fas fa-bars"></i></button>
